0036c74161e73cf8594efb3769c1371fbe086284
Daniel <daniel.agostinho@hotmail.com>
2020-10-31 13:04:38 -0300

    5.4. Validando inconsistências
    5.4. Validando inconsistências

--- src/main/java/br/com/algaworks/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/br/com/algaworks/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.NestedExceptionUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -55,6 +57,17 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
         String mensagemDesenvolvedor = ex.toString();
         List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
         return this.handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity<?> handleDataIntegrityViolationException(
+            DataIntegrityViolationException ex,
+            WebRequest request
+    ){
+        String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = NestedExceptionUtils.getRootCause(ex).getMessage();
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        return this.handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     private List<Erro> criarListaDeErros(BindingResult bindingResult) {
