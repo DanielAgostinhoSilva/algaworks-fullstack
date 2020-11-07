66fa37ac09e611f84c19c84a9beeaa7578da60d8
Daniel <daniel.agostinho@hotmail.com>
2020-11-07 15:04:04 -0300

    6.11. Movendo o usuário para o banco de dados

---
+++ src/main/java/br/com/algaworks/algamoneyapi/model/Usuario.java
@@ -0,0 +1,30 @@
+package br.com.algaworks.algamoneyapi.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private Long codigo;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_permissao", joinColumns = @JoinColumn(name = "codigo_usuario")
+            , inverseJoinColumns = @JoinColumn(name = "codigo_permissao"))
+    private List<Permissao> permissoes;
+}
