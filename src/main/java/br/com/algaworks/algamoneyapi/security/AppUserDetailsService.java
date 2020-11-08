1e758801b22dc0696a946c830f07acd5cfb1f4aa
Daniel <daniel.agostinho@hotmail.com>
2020-11-08 15:34:33 -0300

    7.5. Nome do usuário no token JWT

--- src/main/java/br/com/algaworks/algamoneyapi/security/AppUserDetailsService.java
+++ src/main/java/br/com/algaworks/algamoneyapi/security/AppUserDetailsService.java
@@ -5,7 +5,6 @@ import br.com.algaworks.algamoneyapi.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -24,17 +23,14 @@ public class AppUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Optional<Usuario> optionalUsuario = usuarioRepository.findByEmail(email);
-        Usuario usuario = optionalUsuario.orElseThrow(() ->
-                new UsernameNotFoundException("Usuário e/ou senha incorretos"));
-        return new User(email, usuario.getSenha(), getPermissoes(usuario));
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+        Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+        return new UsuarioSistema(usuario, getPermissoes(usuario));
     }
 
     private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
         Set<SimpleGrantedAuthority> authorities = new HashSet<>();
-        usuario.getPermissoes().forEach(p -> authorities.add(
-                new SimpleGrantedAuthority(p.getDescricao().toUpperCase())
-        ));
+        usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
         return authorities;
     }
 }
